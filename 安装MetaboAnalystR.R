c3dcb56512c2461799ec3b358310778c7cdb1bc6
Likaifeng <likaifeng2011@gmail.com>
2020-12-16 11:15:18 +0800

    安装MetaboAnalystR

---
+++ 安装MetaboAnalystR.R
@@ -0,0 +1,25 @@
+# MetaboAnalystR
+## 安装
+metanr_packages <- function(){
+  metr_pkgs <- c("impute", "pcaMethods", "globaltest", "GlobalAncova", "Rgraphviz", "preprocessCore", "genefilter", "SSPA", "sva", "limma", "KEGGgraph", "siggenes","BiocParallel", "MSnbase", "multtest","RBGL","edgeR","fgsea","devtools","crmn")
+  list_installed <- installed.packages()
+  new_pkgs <- subset(metr_pkgs, !(metr_pkgs %in% list_installed[, "Package"]))
+  if(length(new_pkgs)!=0){if (!requireNamespace("BiocManager", quietly = TRUE))
+    install.packages("BiocManager")
+    BiocManager::install(new_pkgs)
+    print(c(new_pkgs, " packages added..."))
+  }
+  
+  if((length(new_pkgs)<1)){
+    print("No new packages added...")
+  }
+}
+
+metanr_packages()
+
+# Step 1: Install devtools
+
+library(devtools)
+
+# Step 2: Install MetaboAnalystR with documentation
+devtools::install_github("xia-lab/MetaboAnalystR", build = TRUE, build_vignettes = TRUE, build_manual =T)
