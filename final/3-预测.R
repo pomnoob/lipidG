125db7381d9dcde05bb017dad1918a38ad5747e6
Likaifeng <likaifeng2011@gmail.com>
2020-12-29 16:57:37 +0800

    20201229

--- final/3-预测.R
+++ final/3-预测.R
@@ -5,6 +5,11 @@ library(tidyverse)
 library(RANN)
 library(DMwR)
 library(pROC)
+library(mixOmics)
+## install BiocManager if not installed if (!requireNamespace("BiocManager", quietly = TRUE))     install.packages("BiocManager") 
+## install mixOmics 
+BiocManager::install('mixOmics')
+
 
 # 导入标准化后的 T1 & T3 数据
 pred.norm <- read.csv(file = "final/Metab_data_normalized_t_T1&T3.csv",stringsAsFactors = F)
@@ -17,21 +22,8 @@ set.seed(998)
 inTraining <- createDataPartition(pred.norm$group, p = .75, list = FALSE)
 training <- pred.norm[ inTraining,]
 testing  <- pred.norm[-inTraining,]
-# CV
-fitControl <- trainControl(## 10-fold CV
-  method = "LGOCV",
-  number = 20)
-#training
-set.seed(825)
-plsFit1 <- train(group ~ ., data = pred.norm, 
-                 method = "pls", 
-                 trControl = fitControl,
-                 summaryFunction = twoClassSummary)
-plsFit1
 
-pls.pred <- predict(plsFit1, testing, type="prob")
-result.roc <- roc(testing$group, pls.pred$T1)
-plot(result.roc, print.thres="best", print.thres.best.method="closest.topleft")
+
 # 1. upregulated protein
 
 # 2. downregulated protein
---
+++ final/5-mixOmics 分析.R
@@ -0,0 +1,59 @@
+library(tidyverse)
+library(mixOmics)
+library(caret)
+library(pROC)
+# 导入标准化后的 T1 & T3 数据
+pred.norm <- read.csv(file = "final/Metab_data_normalized_t_T1&T3.csv",stringsAsFactors = F)
+pred.norm <- pred.norm %>%
+  dplyr::select(-sample) %>%
+  mutate(group=factor(group,labels = c("T1","T3")))
+
+# All proteins
+# 分割数据
+trainMeats <- createDataPartition(pred.norm$group, p = 2/3,list = F)
+# 准备数据用于pls-da
+X <- as.matrix(pred.norm[-1])
+Y <- pred.norm$group  
+# 训练集和测试集
+Xtrain <- X[ trainMeats, ]
+Ytrain <- Y[ trainMeats ]
+Xtest <- X[ -trainMeats, ]
+Ytest <- Y[ -trainMeats ]
+
+## PLS-DA function
+plsda.train <- plsda(Xtrain, Ytrain, ncomp = 6) # where ncomp is the number of components wanted
+
+# 预测
+plsda.pred <- predict(plsda.train, Xtest, dist = "max.dist")
+
+prediction <- plsda.pred$class$max.dist[,4] 
+
+confusion.mat <- get.confusion_matrix(truth = Ytest, predicted = prediction)
+
+#######################################
+# all data
+
+plsda.res <- plsda(X, Y, ncomp = 10)
+
+set.seed(2543) # for reproducibility here, only when the `cpus' argument is not used
+perf.plsda <- perf(plsda.res, validation = "Mfold", folds = 10, 
+                   progressBar = FALSE, auc = TRUE, nrepeat = 100) 
+# perf.plsda.srbct$error.rate  # error rates
+plot(perf.plsda, col = color.mixo(1:3), sd = TRUE, legend.position = "horizontal")
+
+auc.plsda = mixOmics::auroc(plsda.res, roc.comp = 2)
+
+plotIndiv(plsda.res , comp = 1:2,
+          group = pred.norm$group, ind.names = FALSE, 
+          ellipse = TRUE, legend = TRUE, title = 'PLSDA on SRBCT')
+
+# with background
+background = background.predict(plsda.res, comp.predicted=2, dist = "max.dist") 
+#optional: xlim = c(-40,40), ylim = c(-30,30))
+
+plotIndiv(plsda.res, comp = 1:2,
+          group = pred.norm$group, ind.names = FALSE, title = "Maximum distance",
+          legend = TRUE,  background = background)
+
+p.vip <- vip(plsda.res)
+p.vip$
